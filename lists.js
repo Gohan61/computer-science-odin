ea2ea0f253d1d5a1459a1ffcb6ce13c790a8d0a7
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-03-11 08:48:56 +0100

    Create Linked+new nodes and append function
    Feat: created a new linkedlist class to store the list in
    Feat: created a new node class to create new nodes for the linked list
    Feat: created an append function to append new nodes within the list

---
+++ lists.js
@@ -0,0 +1,16 @@
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+
+  append(value) {
+    nextNode = value;
+  }
+}
+
+class Node {
+  constructor(value = null) {
+    this.value = value;
+    this.nextNode = null;
+  }
+}
