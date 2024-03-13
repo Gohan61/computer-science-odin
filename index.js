4cbd6f523e3a085baef9e3409fcfa5c17489b786
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-03-13 08:11:02 +0100

    Set Hashmap function, hash keys + set key/value
    Feat: set a factory hashmap function that will hold the methods for
    editing the hashmap bucket (also in the function)
    Feat: add method to generate hash keys from keys
    Feat: set hashed key in bucket inside a linked list to prevent
    collisions in the bucket when for example exact same name is used

---
+++ index.js
@@ -0,0 +1,30 @@
+import { LinkedList } from "./linkedList.js";
+import { Node } from "./linkedList.js";
+
+function HashMap() {
+  const hashBucket = [];
+
+  const hash = (key) => {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = hashCode % 16;
+    }
+    return hashCode;
+  };
+
+  const set = (key, value) => {
+    const index = hash(key);
+    if (!hashBucket[index]) {
+      hashBucket[index] = new LinkedList();
+    }
+
+    hashBucket[index].append(new Node([key, value]));
+  };
+
+  return { hash, set, hashBucket };
+}
+
+const hashMapObj = HashMap();
