be43e8e15e490fc326e3df6621628eb4e31f825d
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-03-13 14:29:50 +0100

    Remove unused modules from linkedList class

--- linkedList.js
+++ linkedList.js
@@ -17,16 +17,6 @@ export class LinkedList {
     }
   }
 
-  prepend(value) {
-    if (this.head === null) {
-      this.head = value;
-    } else {
-      let oldHead = this.head;
-      this.head = value;
-      this.head.nextNode = oldHead;
-    }
-  }
-
   size() {
     let count = 0;
     let node = this.head;
@@ -35,52 +25,6 @@ export class LinkedList {
       node = node.nextNode;
     }
     return count;
-  }
-
-  headFirst() {
-    return this.head.value;
-  }
-
-  tail() {
-    let lastNode = this.head;
-    while (lastNode.nextNode !== null) {
-      lastNode = lastNode.nextNode;
-    }
-    return lastNode.value;
-  }
-
-  at(index) {
-    let currentNode = this.head;
-    let count = 0;
-
-    while (currentNode) {
-      if (count === index) {
-        return currentNode.value;
-      }
-      count++;
-      currentNode = currentNode.nextNode;
-    }
-  }
-
-  pop() {
-    if (!this.head) {
-      return null;
-    }
-
-    if (!this.head.nextNode) {
-      this.head = null;
-      return;
-    }
-
-    let previous = this.head;
-    let tail = this.head.nextNode;
-
-    while (tail.nextNode !== null) {
-      previous = tail;
-      tail = tail.nextNode;
-    }
-
-    previous.nextNode = null;
   }
 
   contains(inputValue) {
@@ -120,28 +64,6 @@ export class LinkedList {
       }
       if (currentNode.value[0] === value) {
         return currentNode.value[1];
-      }
-    }
-  }
-
-  findIndex(value) {
-    let currentNode = this.head;
-    let count = 0;
-
-    if (currentNode.value[0] === value) {
-      return count;
-    } else {
-      count++;
-      currentNode = currentNode.nextNode;
-      while (currentNode.value[0] !== value) {
-        currentNode = currentNode.nextNode;
-        if (currentNode === null) {
-          return null;
-        }
-        count++;
-      }
-      if (currentNode.value[0] === value) {
-        return count;
       }
     }
   }
