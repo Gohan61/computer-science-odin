944830dcb8f58a2eba5ec533aa420cd9937f6b07
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-03-17 09:39:38 +0100

    Define merge + mergeSort functions

--- index.js
+++ index.js
@@ -16,3 +16,29 @@ function fibsRec(num, fibsArray = [0, 1]) {
     ]);
   }
 }
+
+function merge(left, right) {
+  let sortedArr = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift())
+    } else {
+      sortedArr.push(right.shift())
+    }
+  }
+  return [...sortedArr, ...left, ...right]
+}
+
+function mergeSort(array) {
+  const middle = array.length / 2;
+
+  if (array.length <= 1) {
+    return array
+  }
+
+  const left = array.splice(0, middle)
+  const right = array
+  return merge(mergeSort(left), mergeSort(right))
+}
+
