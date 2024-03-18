fd129f3d985e822ad860a35f6949200bfbbb4ced
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-03-18 08:46:16 +0100

    Divide buildtree + print functions + add insert
    Feat: separated the buildtree and print functions so functions only have
    one main task
    Fix: since print output was saved to this.root, no Node tree was saved
    to root thus making it inaccessible/editable
    Feat: add an insert function that inserts a new node in the tree

--- index.js
+++ index.js
@@ -9,10 +9,58 @@ class Node {
 class Tree {
   constructor(array) {
     this.array = array;
-    this.root = buildTree;
+    this.root = null;
   }
-}
 
-function buildTree(array) {
-  const data = [...new Set(array)].sort((a, b) => a - b);
+  buildTree() {
+    const sortedArray = [...new Set(this.array)].sort((a, b) => a - b);
+    let start = 0;
+    let end = sortedArray.length - 1;
+
+    function sortedArrayToBst(array, start, end) {
+      if (start > end) {
+        return null;
+      }
+
+      let mid = parseInt((start + end) / 2);
+      let node = new Node(array[mid]);
+
+      node.left = sortedArrayToBst(array, start, mid - 1);
+      node.right = sortedArrayToBst(array, mid + 1, end);
+      return node;
+    }
+
+    return (this.root = sortedArrayToBst(sortedArray, start, end));
+  }
+
+  insert(num, root = this.root) {
+    if (root === null) {
+      root = new Node(num);
+      return (this.root = root);
+    }
+
+    if (num < root.data) {
+      root.left = this.insert(num, root.left);
+    } else if (num > root.data) {
+      root.right = this.insert(num, root.right);
+    }
+
+    return (this.root = root);
+  }
+
+  print(node = this.root) {
+    const prettyPrint = (node, prefix = "", isLeft = true) => {
+      if (node === null) {
+        return;
+      }
+      if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+      }
+      console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+      if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+      }
+    };
+    prettyPrint(node);
+  }
 }
