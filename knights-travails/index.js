0d2ab5f917841203b2177f9400726567b4935716
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-04-17 18:09:13 +0200

    Feat: a search through a graph tree
    * Code is mostly based on other project's code. Even though I went over
    it with the debugger multiple times, this Knights Travails remains a
    mystery to me.

--- knights-travails/index.js
+++ knights-travails/index.js
@@ -1,19 +1,43 @@
-function knightMoves() {
-  const gameBoard = [
-    [0, 1, 2, 3, 4, 5, 6, 7],
-    [0, 1, 2, 3, 4, 5, 6, 7],
-    [0, 1, 2, 3, 4, 5, 6, 7],
-    [0, 1, 2, 3, 4, 5, 6, 7],
-    [0, 1, 2, 3, 4, 5, 6, 7],
-    [0, 1, 2, 3, 4, 5, 6, 7],
-    [0, 1, 2, 3, 4, 5, 6, 7],
-  ];
+function knightMoves(start, end) {
+  const queue = [[start]];
+  const visited = [`${start[0]}${start[1]}`];
 
   const possibleMoves = [
     [1, 2],
     [2, 1],
     [2, -1],
     [1, -2],
-    [-1, -2][(-2, -1)][(1, -2)][(2, -1)],
+    [-1, -2],
+    [-2, -1],
+    [1, -2],
+    [2, -1],
   ];
+
+  while (queue.length > 0) {
+    const path = queue.shift();
+    const current = path[path.length - 1];
+
+    if (current[0] === end[0] && current[1] === end[1]) {
+      return path;
+    }
+    possibleMoves.forEach((move) => {
+      const next = [current[0] + move[0], current[1] + move[1]];
+      const nextString = `${next[0]}${next[1]}`;
+
+      if (stillOnBoard(next[0], next[1]) && !visited.includes(nextString)) {
+        visited.push(nextString);
+
+        queue.push([...path, next]);
+      }
+    });
+  }
+}
+
+console.log(knightMoves([0, 0], [3, 3]));
+
+function stillOnBoard(x, y) {
+  if (x < 0 || x > 7 || y < 0 || y > 7) {
+    return false;
+  }
+  return true;
 }
