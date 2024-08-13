c8ed53cdde3952bb233797ecfb7ea6ee853c3661
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-08-13 15:18:44 +0200

    Refactor: check types with TypeScript

--- Recursion/index.ts
+++ Recursion/index.ts
@@ -1,11 +1,12 @@
-function fibs(num) {
-  const fibsArray = [0, 1];
+function fibs(num: number): number[] {
+  const fibsArray: number[] = [0, 1];
   for (let i = 2; i < num; i++) {
     fibsArray[i] = fibsArray[i - 1] + fibsArray[i - 2];
   }
   return fibsArray;
 }
-function fibsRec(num, fibsArray = [0, 1]) {
+
+function fibsRec(num: number, fibsArray: number[] = [0, 1]): number[] {
   if (fibsArray.length >= num) {
     return fibsArray;
   } else {
@@ -15,23 +16,28 @@ function fibsRec(num, fibsArray = [0, 1]) {
     ]);
   }
 }
-function merge(left, right) {
-  let sortedArr = [];
+
+function merge(left: number[], right: number[]): number[] {
+  let sortedArr: number[] = [];
+
   while (left.length && right.length) {
     if (left[0] < right[0]) {
-      sortedArr.push(left.shift());
+      sortedArr.push(left.shift()!);
     } else {
-      sortedArr.push(right.shift());
+      sortedArr.push(right.shift()!);
     }
   }
   return [...sortedArr, ...left, ...right];
 }
-function mergeSort(array) {
-  const middle = array.length / 2;
+
+function mergeSort(array: number[]): number[] {
+  const middle: number = array.length / 2;
+
   if (array.length <= 1) {
     return array;
   }
-  const left = array.splice(0, middle);
-  const right = array;
+
+  const left: number[] = array.splice(0, middle);
+  const right: number[] = array;
   return merge(mergeSort(left), mergeSort(right));
 }
